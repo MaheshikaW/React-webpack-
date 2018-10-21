e8f0ede0d933ad76ed5b15d8502739ddfc1d1b3e
Maheshika123 <maheshiweera11@gmail.com>
2018-10-21 22:12:53 +0530

    new changes

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+import {Footer} from './Footer';
+
+class App extends Component {
+    render(){
+        return(
+        <div>
+            <h1>MY APP!!</h1>
+            <Footer />
+            </div>
+        );}
+}
+
+export default App;
